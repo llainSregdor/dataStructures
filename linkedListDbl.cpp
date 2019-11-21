c3227814cab4c2c047a8e12827990f878a77c73e
Niall Rodgers <43591426+llainSregdor@users.noreply.github.com>
2019-11-21 15:53:31 +1100

    Add files via upload

---
+++ linkedListDbl.cpp
@@ -0,0 +1,45 @@
+//*************************************************************
+//DOUBLY LINKED LIST:
+//*************************************************************
+
+struct list_t
+{
+	struct node_t
+	{   node_t *prev;
+		node_t *next;
+	    void *data;
+	};
+
+	node_t anchor;
+
+	dbllist()
+	{	anchor.next = &anchor;
+		anchor.next = &anchor;
+	}
+
+	void add(void *data)
+	{//function to add new node.
+		node_t *node = new node_t();
+		node->data = data;
+		node->next = anchor.next;
+		node->next->prev = node;
+		node->prev = &anchor;
+		anchor.next = node;
+	}
+
+	void printAll()
+	{//function to print all nodes
+		for (node_t *node=anchor.next; node != anchor; node=node->next)
+		{printTheNode(node->data);
+		}
+	}
+
+	void del()
+	{//function to delete a node
+		node->next->prev = node->prev;
+		node->prev->next = node->next;
+		freeTheData(node->data);
+		delete node;
+
+	}
+}
